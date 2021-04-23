72c796195345f0e9e6a449b559b4f79f60c80e4c
chasebw <chasebwilcox@gmailcom>
2021-04-23 15:44:16 -0600

    Testing Login and Home Page on Heroku

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'build')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/heroku', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
